727b0d61e0d6a87cb38d538d80d9acc51006e8a6
Johnavan Thomas <johnavanthomas@gmail.com>
2020-02-27 20:00:35 -0500

    Added the API and search bar

--- Fridgey/src/screens/RecipeScreen.js
+++ Fridgey/src/screens/RecipeScreen.js
@@ -1,13 +1,28 @@
-import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { SearchBar } from "react-native-elements";
 
 const RecipeScreen = () => {
-	return (
-		<View>
-			<Text>Recipe</Text>
-		</View>
-	)
-}
+  const API_KEY = "dedef814c2f44537b5c5c16d6c993941";
+  const exampleReq = `https://api.spoonacular.com/recipes/search?query=cheese&apiKey=${API_KEY}`;
+
+  const getRecipes = async () => {
+    const response = await fetch(exampleReq);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    console.log("working");
+    getRecipes();
+  }, []);
+
+  return (
+    <View>
+      <SearchBar> Search</SearchBar>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({});
 
