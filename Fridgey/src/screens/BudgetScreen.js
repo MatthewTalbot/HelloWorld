dd886c410fb539b51e01094c84d0638bc183ee03
Matthew Talbot <matthew.ian.talbot@gmail.com>
2020-03-20 13:30:10 -0400

    added component for the budget screen

--- Fridgey/src/screens/BudgetScreen.js
+++ Fridgey/src/screens/BudgetScreen.js
@@ -1,13 +1,25 @@
-import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import React, {useState} from "react";
+import { Text, TextInput, StyleSheet, View } from "react-native";
+import BudgetInput from '../components/Budget/BudgetInput'
 
+const displayBudget = (budget) => {
+	return <Text>budget</Text>
+}
 const BudgetScreen = () => {
+	const [budget, setBudget] = useState(0)
 	return (
 		<View>
-			<Text>Budget</Text>
+			<Text>Enter Monthly Budget</Text>
+			<BudgetInput
+				budget = {budget}
+				onBudgetChange = {(newBudget) => setBudget(newBudget)}
+				onBudgetSubmit = {{<Text>{budget}</Text>}}
+			/>
 		</View>
 	)
 }
+
+
 
 const styles = StyleSheet.create({});
 
