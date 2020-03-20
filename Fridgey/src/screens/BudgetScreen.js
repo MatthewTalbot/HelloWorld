2074e399eaa78a8f7c853274d7cb6a042f9b1ef7
Matthew Talbot <matthew.ian.talbot@gmail.com>
2020-03-20 14:25:01 -0400

    reverted some changes

--- Fridgey/src/screens/BudgetScreen.js
+++ Fridgey/src/screens/BudgetScreen.js
@@ -2,19 +2,26 @@ import React, {useState} from "react";
 import { Text, TextInput, StyleSheet, View } from "react-native";
 import BudgetInput from '../components/Budget/BudgetInput'
 
-const displayBudget = (budget) => {
-	return <Text>budget</Text>
-}
 const BudgetScreen = () => {
 	const [budget, setBudget] = useState(0)
+	const [foodWaste, setFoodWaste] = useState(0)
+	const [spending, setSpending] = useState(0)
 	return (
 		<View>
 			<Text>Enter Monthly Budget</Text>
 			<BudgetInput
 				budget = {budget}
 				onBudgetChange = {(newBudget) => setBudget(newBudget)}
-				onBudgetSubmit = {{<Text>{budget}</Text>}}
+				display = "Budget"
 			/>
+		<BudgetInput
+			budget = {spending}
+			onBudgetChange = {(newSpending) => setSpending(newSpending)}
+			onBudgetSubmit = {() => setBudget(budget - spending)}
+			display = "Spent on Food"
+		/>
+		<Text>Remaining Budget: {budget}</Text>
+		<Text>Wasted Food: {foodWaste}</Text>
 		</View>
 	)
 }
